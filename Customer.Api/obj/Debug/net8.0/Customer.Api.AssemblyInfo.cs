9c68aef0f0c98f1a65ca8b2ff4c3722c33d131f5
yashshah <yashshah7575@gmail.com>
2025-06-09 11:40:22 -0500

    init

--- Customer.Api/obj/Debug/net8.0/Customer.Api.AssemblyInfo.cs
+++ Customer.Api/obj/Debug/net8.0/Customer.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customer.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4462a4e666a6854bbddaaf6225f037e98ec7243a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customer.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customer.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Customer.Common/obj/Debug/net8.0/Customer.Common.AssemblyInfo.cs
+++ Customer.Common/obj/Debug/net8.0/Customer.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Customer.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4462a4e666a6854bbddaaf6225f037e98ec7243a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Customer.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Customer.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
